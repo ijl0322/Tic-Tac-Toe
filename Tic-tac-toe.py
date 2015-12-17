418dfe9984e3de7227a913ba1ae00b8efd02877f
Isabel Lee <ijl0322@gmail.com>
2015-12-17 18:22:19 +0800

    playing board implemented

---
+++ Tic-tac-toe.py
@@ -0,0 +1,39 @@
+import pygame, sys, time, random
+from pygame.locals import *
+#from moves import *
+
+pygame.init()
+window = pygame.display.set_mode((450, 550))
+pygame.display.set_caption("Tic-Tac-Toe")
+
+WHITE = (255,255,255)
+GREEN = (77,204,0)
+RED = (255,0,0)
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+myfont = pygame.font.SysFont(None, 50)
+
+blocks = []
+for i in range(3):
+    for j in range(3):
+        blocks.append([pygame.Rect((i*130) + 35 , (j*130) + 135, 120, 120), WHITE])
+
+while True:
+    
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    window.fill(WHITE)
+    header = myfont.render("Tic-Tac-Toe", True, RED)
+    window.blit(header, (30, 50))
+    pygame.draw.rect(window, BLACK, pygame.Rect(25, 125, 400, 400))
+    
+    for block in blocks:
+        pygame.draw.rect(window, block[1], block[0])
+        
+    pygame.display.update()
+
+    time.sleep(0.02)
